d7888f8f355fb787301cf95b84c7b4754de116ed
Wilfried Ladenhauf <wladenhauf@gmail.com>
2022-11-01 05:19:49 +0100

    add local tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.{html}",
+    "./impressum.{html}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
